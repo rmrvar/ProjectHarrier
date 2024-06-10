86cc6504eb8260b72270536aec4fd2aec359cba3
rmrvar <113578872+rmrvar@users.noreply.github.com>
2024-06-11 00:23:10 +0200

    Added anti gravity mushrooms and 2d character controller.

---
+++ Assets/Scripts/AntiGravityMushroom.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Platformer.Mechanics;
+using UnityEngine;
+
+public class AntiGravityMushroom : MonoBehaviour
+{
+    // TODO: If IsActive is set to true, have to retrigger collisions (overlap a box and do the same thing as in OnCollisionEnter2D).
+    public bool IsActive { get; private set; } = true;
+
+    public void Activate()
+    {
+        IsActive = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("OnTriggerEnter2D");
+        var controller = collider.GetComponent<PlayerController>();
+        if (controller == null)
+        {
+            return;
+        }
+
+        if (IsActive)
+        {
+            controller.InvertGravity = true;
+            controller.StopJump();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        var controller = collider.GetComponent<PlayerController>();
+        if (controller == null)
+        {
+            return;
+        }
+
+        if (IsActive)
+        {
+            controller.InvertGravity = false;
+        }
+    }
+}
